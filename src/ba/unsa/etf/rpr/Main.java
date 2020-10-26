13a909c1f001b87f8e5eaa3f2e5d4f6579dd10a3
dnuhic <dnuhic1@etf.unsa.ba>
2020-10-26 19:00:18 +0100

    Zavrsen zadatak rpr-t1-z3

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -3,6 +3,13 @@ package ba.unsa.etf.rpr;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Sat s = new Sat(15,30,45);
+        s.Ispisi();
+        s.sljedeci();
+        s.Ispisi();
+        s.pomjeriZa(-48);
+        s.Ispisi();
+        s.postavi(0,0,0);
+        s.Ispisi();
     }
 }
